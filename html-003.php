76659360465dd0bb1ba3ef48210f59156aa6a948
dangnguyenhocdev <91862995+dangnguyenhtml@users.noreply.github.com>
2023-07-22 17:19:34 +0700

    Update html-003.php

--- html-003.php
+++ html-003.php
@@ -17,7 +17,7 @@ if (isset($_GET['p'])){
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Bài tập lớn nhóm 15</title>
+<title>Bao moi</title>
 <link rel = "preconnect" href = "https://fonts.googleapis.com">
 <link rel = "preconnect" href = "https://fonts.gstatic.com" crossorigin>
 <link rel="stylesheet" href="CSS-003.css">
